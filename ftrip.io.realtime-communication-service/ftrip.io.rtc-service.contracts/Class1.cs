9e38efc934d479dfbcc6b0d79cbd22990e530f6e
Panic <panic.milos99@gmail.com>
2023-05-05 23:23:59 +0200

    feat: framework setup, dockerfiles

--- ftrip.io.realtime-communication-service/ftrip.io.rtc-service.contracts/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ftrip.io.rtc_service.contracts
-{
-    public class Class1
-    {
-    }
-}
--- ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Startup.cs
+++ ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Startup.cs
@@ -1,15 +1,17 @@
+using ftrip.io.framework.auth;
+using ftrip.io.framework.ExceptionHandling.Extensions;
+using ftrip.io.framework.HealthCheck;
+using ftrip.io.framework.Installers;
+using ftrip.io.framework.Mapping;
+using ftrip.io.framework.messaging.Installers;
+using ftrip.io.framework.Secrets;
+using ftrip.io.framework.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ftrip.io.rtc_service
 {
@@ -26,6 +28,15 @@ namespace ftrip.io.rtc_service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            InstallerCollection.With(
+                new HealthCheckUIInstaller(services),
+                new AutoMapperInstaller<Startup>(services),
+                new FluentValidationInstaller<Startup>(services),
+                new EnviromentSecretsManagerInstaller(services),
+                new JwtAuthenticationInstaller(services),
+                new RabbitMQInstaller<Startup>(services, RabbitMQInstallerType.Publisher | RabbitMQInstallerType.Consumer)
+            ).Install();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,12 +51,22 @@ namespace ftrip.io.rtc_service
 
             app.UseRouting();
 
+            app.UseCors(policy => policy
+               .WithOrigins(Environment.GetEnvironmentVariable("API_PROXY_URL"))
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+            );
+
             app.UseAuthorization();
+
+            app.UseFtripioGlobalExceptionHandler();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseFtripioHealthCheckUI(Configuration.GetSection(nameof(HealthCheckUISettings)).Get<HealthCheckUISettings>());
         }
     }
 }
