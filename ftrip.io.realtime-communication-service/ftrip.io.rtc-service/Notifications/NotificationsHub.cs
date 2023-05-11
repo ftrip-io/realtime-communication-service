86c40d46579b5aa19c88df36c9f1bc9d07ea3a48
Panic <panic.milos99@gmail.com>
2023-05-11 15:06:22 +0200

    feat: #1 SignalR setup and sending messages to notifications hub by consumer

---
+++ ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Notifications/NotificationsHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ftrip.io.rtc_service.Notifications
+{
+    [Authorize]
+    public class NotificationsHub : Hub
+    {
+        public static string Endpoint = "/hubs/notifications";
+    }
+}
