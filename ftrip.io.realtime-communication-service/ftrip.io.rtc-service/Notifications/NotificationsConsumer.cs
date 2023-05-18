3a833eb2d80f4c9a2e184e4d2919cd1e66ad4003
Panic <panic.milos99@gmail.com>
2023-05-18 21:59:56 +0200

    Feat/logging (#6)
    * feat: logging setup and logging messages
    
    * feat: updated framework to 2.5.1

--- ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Notifications/NotificationsConsumer.cs
+++ ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Notifications/NotificationsConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using ftrip.io.notification_service.contracts.Notifications.Events;
 using MassTransit;
 using Microsoft.AspNetCore.SignalR;
+using Serilog;
 using System.Threading.Tasks;
 
 namespace ftrip.io.rtc_service.Notifications
@@ -8,15 +9,21 @@ namespace ftrip.io.rtc_service.Notifications
     public class NotificationsConsumer : IConsumer<NotificationSavedEvent>
     {
         private readonly IHubContext<NotificationsHub> _notificationsHubContext;
+        private readonly ILogger _logger;
 
-        public NotificationsConsumer(IHubContext<NotificationsHub> notificationsHubContext)
+        public NotificationsConsumer(
+            IHubContext<NotificationsHub> notificationsHubContext,
+            ILogger logger)
         {
             _notificationsHubContext = notificationsHubContext;
+            _logger = logger;
         }
 
         public async Task Consume(ConsumeContext<NotificationSavedEvent> context)
         {
             var notificationSaved = context.Message;
+
+            _logger.Information("Sending a message to the Notification Hub - UserId[{UserId}]", notificationSaved.UserId);
 
             await _notificationsHubContext.Clients
                 .User(notificationSaved.UserId)
--- ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Notifications/NotificationsHub.cs
+++ ftrip.io.realtime-communication-service/ftrip.io.rtc-service/Notifications/NotificationsHub.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Serilog;
+using System;
+using System.Threading.Tasks;
 
 namespace ftrip.io.rtc_service.Notifications
 {
@@ -7,5 +10,32 @@ namespace ftrip.io.rtc_service.Notifications
     public class NotificationsHub : Hub
     {
         public static string Endpoint = "/hubs/notifications";
+
+        private readonly ILogger _logger;
+
+        public NotificationsHub(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            _logger.Information(
+                "New connection to the Notifications Hub - ConnectionId[{ConnectionId}], UserId[{UserId}]",
+                Context.ConnectionId, Context.UserIdentifier
+            );
+
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            _logger.Information(
+                "Connection disconnected from the Notifications Hub - ConnectionId[{ConnectionId}], UserId[{UserId}]",
+                Context.ConnectionId, Context.UserIdentifier
+            );
+
+            return base.OnDisconnectedAsync(exception);
+        }
     }
 }
